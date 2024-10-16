d9f005986ddab508bbb739c97c4bffacbd393d4f
NyCore <122640663+NyCores@users.noreply.github.com>
2024-10-16 13:40:03 -0400

    Delete Website directory

--- Website/paywall.js
+++
@@ -1,5 +0,0 @@
-// JavaScript instructions to handle subscription button click
-document.getElementById("subscribe-btn").addEventListener("click", function() {
-    // Redirect users to subscription page
-    window.location.href = "subscribe.html";
-});
